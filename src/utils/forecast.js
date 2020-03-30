04c01762b93a9d6adac641da413bf1245c815035
unknown <vaibhavgupta305@gmail.com>
2020-03-30 13:09:02 +0530

    Init changes

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request=require('request')
+const forecast=(a,b,callback)=>{
+    const url='https://api.darksky.net/forecast/cb0adbab53df73de6a137e5a4b966801/'+a+','+b
+    request({url,json:true},(error,{body})=>{
+        if(error)
+        {
+            callback('Unable to connect to weather services!',undefined)
+        }
+        else if(body.error)
+        {
+           callback('Unable to find location',undefined)
+        }
+        else{
+            callback(undefined,body.daily.data[0].summary+'It is currently'+body.currently.temperature+'degrees out. There is a'+body.currently.precipProbability+'% chance of rain')
+        }
+    })
+}
+module.exports=forecast
